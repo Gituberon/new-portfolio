97d4fc934794c6b0350bb90b5556dbc296b29be7
Amirxonoff <jumasevino@gmail.com>
2023-12-06 23:57:02 +0500

    kun-tun rajimini yangilash

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 const slides = document.querySelectorAll(".slide");
-
-// loop through slides and set each slides translateX property to index * 100% 
 slides.forEach((slide, indx) => {
   slide.style.transform = `translateX(${indx * 100}%)`;
 });
-
 const nextSlide = document.querySelector(".btn-next");
-
-// current slide counter
 let curSlide = 0;
-// maximum number of slides
 let maxSlide = slides.length - 1;
-
-// add event listener and navigation functionality
 nextSlide.addEventListener("click", function () {
-  // check if current slide is the last and reset current slide
   if (curSlide === maxSlide) {
     curSlide = 0;
   } else {
     curSlide++;
   }
-
-//   move slide by -100%
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
 });
+function toggle_light_mode() {
+  var app = document.getElementsByTagName("BODY")[0];
+  if (localStorage.lightMode == "dark") {
+    localStorage.lightMode = "light";
+    app.setAttribute("light-mode", "light");
+  } else {
+    localStorage.lightMode = "dark";
+    app.setAttribute("light-mode", "dark");
+  }
+}
+window.addEventListener(
+  "storage",
+  function () {
+    if (localStorage.lightMode == "dark") {
+      app.setAttribute("light-mode", "dark");
+    } else {
+      app.setAttribute("light-mode", "light");
+    }
+  },
+  false
+);
